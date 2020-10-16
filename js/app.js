a3663eddd2cb7142783fbd720d12a73e93c6485c
obadaalmatrami92 <obada.matrami92@gmail.com>
2020-10-17 02:24:02 +0300

    fix

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 var firstname = prompt('Hello , what is your name ? ');
@@ -6,42 +5,41 @@ alert(' Nice to meet you :' + firstname);
 console.log('name', firstname);
 document.write('<h1>' + firstname + '</h1>');
 var counter = 0;
-=======
-'use strict';//use with string datatype
-var love = prompt('Do obada love coding?')
-love = love.toLowerCase();
-if (love == 'yes') {
-    alert('Of course they do');
-} else if (love == 'y') {
-    alert('Of course they do');
-} else if (love == 'no') {
-    alert('Ops, wrong annswer , obada love coding');
-} else if (love == 'n') {
-    alert('Ops, wrong annswer , obada love coding');
-}
-else {
-    alert('please refresh the page and re enter the yes or no answers');
-}
-console.log('love' + love);
 
 
-var car = prompt('which is the most car company obada love ? is BMW')
-car = car.toLowerCase();
-if (car == 'yes') {
-    alert('Of course they do do');
-} else if (car == 'y') {
-    alert('Of course they do');
-} else if (car == 'no') {
-    alert('Ops, wrong annswer , obada love  BMW ');
-} else if (car == 'n') {
-    alert('Ops, wrong annswer , obada love  BMW ');
-}
-else {
-    alert('please refresh the page and re enter the yes or no answers');
-}
+// var love = prompt('Do obada love coding?');
+// love = love.toLowerCase();
+// if (love == 'yes') {
+//     alert('Of course they do');
+// } else if (love == 'y' || love== 'yes') {
+//     alert('Of course they do');
+// } else if (love == 'no') {
+//     alert('Ops, wrong annswer , obada love coding');
+// } else if (love == 'n') {
+//     alert('Ops, wrong annswer , obada love coding');
+// }
+// else {
+//     alert('please refresh the page and re enter the yes or no answers');
+// }
+// console.log('love' + love);
 
-console.log('car' + car);
->>>>>>> master
+
+// var car = prompt('which is the most car company obada love ? is BMW');
+// car = car.toLowerCase();
+// if (car == 'yes') {
+//     alert('Of course they do do');
+// } else if (car == 'y') {
+//     alert('Of course they do');
+// } else if (car == 'no') {
+//     alert('Ops, wrong annswer , obada love  BMW ');
+// } else if (car == 'n') {
+//     alert('Ops, wrong annswer , obada love  BMW ');
+// }
+// else {
+//     alert('please refresh the page and re enter the yes or no answers');
+// }
+
+// console.log('car' + car);
 
 function question1(userResponse) {
 
@@ -118,87 +116,40 @@ function question1(userResponse) {
   document.write('<h1>' + userResponse + '</h1>');
   return userResponse;
 
-
-<<<<<<< HEAD
-=======
-var  g1 = prompt('when the first time obada drive his first car ? in 2004 ')
-g1 = car.toLowerCase();
-if (g1 == 'yes') {
-    alert('correct , will done');
-} else if (g1 == 'y') {
-    alert('correct , will done');
-} else if (g1 == 'no') {
-    alert('Ops, wrong answer ');
-} else if (g1 == 'n') {
-    alert('Ops, wrong annswer');
 }
-else {
-    alert('please refresh the page and re enter the yes or no answers');
-}
-console.log('g1' + g1);
-
-
-var j2 = prompt('which is the most country do obada love  --is jordan ')
-j2 = j2.toLowerCase();
-if (j2 == 'yes') {
-    alert('good , obada love his country , "jordan"');
-} else if (j2 == 'y') {
-    alert('good , obada love his country , "jordan"');
-} else if (j2 == 'no') {
-    alert('Ops, wrong annswer');
-} else if (j2 == 'n') {
-    alert('Ops, wrong annswer');
-}
-else {
-    alert('please refresh the page and re enter the yes or no answers');
->>>>>>> master
-}
-
 question1();
 
 
-<<<<<<< HEAD
 function q6(age) {
   for (var f = 0; f < 5; f++) {
     // eslint-disable-next-line no-redeclare
     age = prompt('how old is obada ? Please Make sure to enter a number');
-=======
-console.log('j2' + j2);
->>>>>>> master
+    // console.log('j2' + j2);
 
-    if (age === 27) {
-      alert(' correct answer !');
-      counter++;
-      console.log(age);
-      break;
-    } else if (age > 27) {
+    if (age > 27) {
       alert('oops , too high');
       console.log(age);
     } else if (age < 27) {
       alert(' oops , too low ');
       console.log(age);
+    // eslint-disable-next-line eqeqeq
+    }else if (age == 27) {
+      alert(' correct answer !');
+      counter++;
+      console.log(age);
+      break;
+    }
+    else{
+      alert('Make sure to enter a number');
     }
   }
   alert('you are right how you guess the answer ');
   document.write('<h1>' + age + '</h1>');
 
-<<<<<<< HEAD
   return (age);
-=======
-var d1 = prompt('where does obada live ? in Jordan ')
-d1 = d1.toLowerCase();
-if (d1 == 'yes') {
-    alert('yes obada living in jordan');
-} else if (d1 == 'y') {
-    alert('yes obada living in jordan');
-} else if (d1 == 'no') {
-    alert('Ops, wrong annswer, obada live in jordan');
-} else if (d1 == 'n') {
-    alert('Ops, wrong annswer, obada live in jordan');
->>>>>>> master
+
 }
 q6();
-
 function question7() {
   var country = ['Maldive', 'Turkey', 'Brazil', 'Jordan-petra', 'New York City', 'USA', 'Italy'];
   for (var t = 0; t < 6; t++) {
@@ -228,12 +179,5 @@ function question7() {
   }
 
 }
-<<<<<<< HEAD
-// eslint-disable-next-line new-cap
 question7();
-alert('Thank you  you got this point ' + counter);
-// console.log('Question7');
-
-=======
-console.log('d1' + d1);
->>>>>>> master
+alert('congrats ' + (firstname) + ' you got this score  ' + counter + '/7'  );
